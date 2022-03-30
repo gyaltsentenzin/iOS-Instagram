d84f9adec0d55b6819ef23d8fd6e180884411d40
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-03-30 18:54:11 -0500

    Created Fake comments

--- iOS Instagram/FeedViewController.swift
+++ iOS Instagram/FeedViewController.swift
@@ -64,6 +64,26 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("comment saved")
+            } else {
+                print("Error saving comment")
+            }
+            
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
@@ -74,5 +94,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate =  windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
 }
